3c69bcad303ff601f30303283cb3ec4de9d00adb
Octave Raimbault <dispyx@gmail.com>
2016-09-26 00:37:09 +0200

    Updated dist

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,31 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-exports.default = deepDiff;
+function arrayEquals(arr1, arr2) {
+  if (!(arr1 instanceof Array)) {
+    throw new TypeError('arrayEqual 1st parameter must be an array');
+  }
+
+  if (!(arr2 instanceof Array)) {
+    return false;
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (var i = 0, l = arr1.length; i < l; i++) {
+    if (arr1[i] instanceof Array && arr2[i] instanceof Array) {
+      if (!arr1[i].equals(arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function deepDiff(obj1, obj2) {
   var keepNewKeys = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
 
@@ -19,15 +43,22 @@ function deepDiff(obj1, obj2) {
   }
 
   var diff = {};
-  var keep = Boolean(keepNewKeys);
 
   Object.keys(obj2).forEach(function (key) {
-    if (!obj1[key] && !keep) {
+    if (!obj1.hasOwnProperty(key) && !keepNewKeys) {
       return;
     }
 
     if (_typeof(obj2[key]) === 'object') {
-      diff[key] = deepDiff(obj1[key], obj2[key], keep);
+      if (obj2[key] instanceof Array) {
+        if (!arrayEquals(obj2[key], obj1[key])) {
+          diff[key] = obj2[key];
+        }
+
+        return;
+      }
+
+      diff[key] = deepDiff(obj1[key], obj2[key], keepNewKeys);
       return;
     }
 
@@ -39,3 +70,5 @@ function deepDiff(obj1, obj2) {
 
   return diff;
 }
+
+exports.default = deepDiff;
