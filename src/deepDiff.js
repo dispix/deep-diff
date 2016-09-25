42c2cfecb98fb3e63e967cc51fd27c2c41ea5039
Octave Raimbault <dispyx@gmail.com>
2016-09-26 00:35:41 +0200

    Fixed array comparison

--- src/deepDiff.js
+++ src/deepDiff.js
@@ -1,4 +1,29 @@
-export default function deepDiff (obj1, obj2, keepNewKeys = false) {
+function arrayEquals (arr1, arr2) {
+  if (!(arr1 instanceof Array)) {
+    throw new TypeError('arrayEqual 1st parameter must be an array')
+  }
+
+  if (!(arr2 instanceof Array)) {
+    return false
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (var i = 0, l = arr1.length; i < l; i++) {
+    if (arr1[i] instanceof Array && arr2[i] instanceof Array) {
+      if (!arr1[i].equals(arr2[i])) {
+        return false
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function deepDiff (obj1, obj2, keepNewKeys = false) {
   if (typeof obj1 !== 'object') {
     throw new TypeError('First parameter must be an object')
   }
@@ -15,6 +40,14 @@ export default function deepDiff (obj1, obj2, keepNewKeys = false) {
     }
 
     if (typeof obj2[key] === 'object') {
+      if (obj2[key] instanceof Array) {
+        if (!arrayEquals(obj2[key], obj1[key])) {
+          diff[key] = obj2[key]
+        }
+
+        return
+      }
+
       diff[key] = deepDiff(obj1[key], obj2[key], keepNewKeys)
       return
     }
@@ -27,3 +60,5 @@ export default function deepDiff (obj1, obj2, keepNewKeys = false) {
 
   return diff
 }
+
+export default deepDiff
