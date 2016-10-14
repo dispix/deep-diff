9b294785b024273527af0870e9dc753a99475cfe
Octave Raimbault <dispyx@gmail.com>
2016-10-14 18:04:22 +0200

    Moved arrayEquals function to utils/arrayEquals.js

---
+++ src/utils/arrayEquals.js
@@ -0,0 +1,34 @@
+/**
+ *
+ *  Checks if two arrays are strictly equals
+ *  @method  arrayEquals
+ *  @param   {Array}     arr1  First array
+ *  @param   {Array}     arr2  Second array, will be compared to arr1
+ *  @return  {Boolean}         True if array are equals, otherwise false
+ */
+function arrayEquals (arr1, arr2) {
+  if (!(arr1 instanceof Array)) {
+    throw new TypeError('arrayEqual 1st parameter must be an array')
+  }
+
+  if (!(arr2 instanceof Array)) {
+    return false
+  }
+
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (var i = 0, l = arr1.length; i < l; i++) {
+    if (arr1[i] instanceof Array && arr2[i] instanceof Array) {
+      if (!arrayEquals(arr1[i], arr2[i])) {
+        return false
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+export default arrayEquals
