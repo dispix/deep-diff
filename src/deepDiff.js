3825f023fffb51c0b109e8b834c448f4f87e5164
Octave Raimbault <dispyx@gmail.com>
2016-10-14 17:50:50 +0200

    Added jsDoc to the sources

--- src/deepDiff.js
+++ src/deepDiff.js
@@ -1,3 +1,11 @@
+/**
+ *
+ *  Checks if two arrays are strictly equals
+ *  @method  arrayEquals
+ *  @param   {Array}     arr1  First array
+ *  @param   {Array}     arr2  Second array, will be compared to arr1
+ *  @return  {Boolean}         True if array are equals, otherwise false
+ */
 function arrayEquals (arr1, arr2) {
   if (!(arr1 instanceof Array)) {
     throw new TypeError('arrayEqual 1st parameter must be an array')
@@ -23,6 +31,18 @@ function arrayEquals (arr1, arr2) {
   return true
 }
 
+/**
+ *
+ *  Return an object containing the keys that have different values in
+ *  two structuraly similar objects.
+ *  @method  deepDiff
+ *  @param   {Object}   obj1                 First object
+ *  @param   {Object}   obj2                 Second object, keys that have different values
+ *                                            from their counterpart in obj1 will be returned
+ *  @param   {Boolean}  [keepNewKeys=false]  If set to true, keys that doesn't exist in obj2
+ *                                            but exist in obj2 will be returned.
+ *  @return  {Object}                        A diff object
+ */
 function deepDiff (obj1, obj2, keepNewKeys = false) {
   if (typeof obj1 !== 'object') {
     throw new TypeError('First parameter must be an object')
