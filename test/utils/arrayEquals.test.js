ec2a5e5600986cee5f9f9be8d7e18faa54744f03
Octave Raimbault <dispyx@gmail.com>
2016-10-23 22:48:05 +0200

    Added tests for the arrayEquals method

---
+++ test/utils/arrayEquals.test.js
@@ -0,0 +1,44 @@
+import arrayEquals from '../../src/utils/arrayEquals'
+
+describe('arrayEquals', () => {
+  const arrOne = [
+    1,
+    {'two': 2},
+    true,
+    [1, 2, 3]
+  ]
+
+  const arrTwo = [
+    1,
+    {'three': 2},
+    true,
+    [1, 2, 3]
+  ]
+
+  const arrThree = [
+    1,
+    {'three': 2}
+  ]
+
+  const arrFour = [
+    1,
+    {'three': 2},
+    true,
+    [1, 2, 3]
+  ]
+
+  it('should return true if the arrays are different', () => {
+    expect(arrayEquals(arrOne, arrTwo)).toEqual(false)
+    expect(arrayEquals(arrOne, arrThree)).toEqual(false)
+    expect(arrayEquals(arrOne, arrFour)).toEqual(false)
+    expect(arrayEquals(arrOne, true)).toEqual(false)
+  })
+
+  it('should return false if the arrays are equals', () => {
+    expect(arrayEquals(arrOne, arrOne)).toEqual(true)
+  })
+
+  it('should throw an error if the first parameter is not an array', () => {
+    expect(() => arrayEquals({}, arrOne)).toThrowError(TypeError)
+  })
+})
